5609579db9d666e6f84a01754daff20e279ecf73
Jayke Peters <jaykepeters@gmail.com>
2020-02-24 01:20:24 -0600

    Create config.php

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+// UniFi Controller Information
+define('controller_url', 'https://unifi.mydomain.com:8443');
+define('controller_version', '5.12.35');
+
+// Unifi Controller Authentication
+define('controller_user', 'ubnt');
+define('controller_password','ubnt');
+
+// Your preferred site
+define('site_id', 'abc0d1e2');
+
+// User pin for safety
+define('PIN', 1234);
